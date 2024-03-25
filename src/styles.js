5bfd3e6df7c593841edc4b9d51a0bda142adb36e
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-03-25 12:04:13 -0300

    Ajustes na fonte

--- src/styles.js
+++ src/styles.js
@@ -4,6 +4,7 @@ export default createGlobalStyle`
 *,body{
     margin: 0;
     padding: 0;
+    font-family: Inter,'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
 body{
     background-color: #22272e;
